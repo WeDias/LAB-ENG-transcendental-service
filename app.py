762047eeeff62191355e7aae7ca186e95ded74f2
Wesley Dias <56437612+WeDias@users.noreply.github.com>
2021-09-09 11:59:48 -0300

    add transcendental-service

---
+++ app.py
@@ -0,0 +1,17 @@
+from math import cos
+from flask import Flask, request
+from flask_cors import cross_origin
+
+
+app = Flask(__name__)
+
+
+@app.route('/transcendental', methods=['POST'])
+@cross_origin()
+def post_result():
+    try:
+        result = cos(float(request.form["value1"]))
+    except Exception as error:
+        return {'status': 500, 'error': 'Ocorreu um erro ao processar a requisição'}
+    else:
+        return {'status': 200, 'result': result}
